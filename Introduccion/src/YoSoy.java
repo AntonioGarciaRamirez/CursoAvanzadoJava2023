0cabd841590fae5770fdbce0ac61a7b54d6e07d8
AntonioGarciaRamirez <antgarramm@gmail.com>
2023-02-24 12:41:06 +0100

    Dwn

---
+++ Introduccion/src/YoSoy.java
@@ -0,0 +1,8 @@
+public class YoSoy {
+    public static void main(String[] args){
+        //Imprime el nombre y el curso que realizo en la consola (RUN)
+//        System.out.println("Yo soy Antonio");
+//        System.out.println("Estoy realizando el curso avanzado de JAVA");
+        System.out.println("Yo soy Antonio\nY estoy en el curso avanzado de JAVA"); //Atajo: poner "\n" para salto linea
+    }
+}
